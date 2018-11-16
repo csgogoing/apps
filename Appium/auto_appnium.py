f7d04c2cc076540c724e54f661ea4c2ba209ccf7
wangpan <daniel_lawliet@qq.com>
2018-11-16 11:16:24 +0800

    提交半完成的APP自动化测试代码
    由于APP项目移交，暂停优化

--- Appium/auto_appnium.py
+++ Appium/auto_appnium.py
@@ -4,23 +4,24 @@ import re
 import time
 from appium import webdriver
 global driver
-import random
+import os
+import sys
 
 
 class Test_appnium:
 	def __init__(self):
 		# 读取设备 id
-		readDeviceId = list(os.popen('adb devices').readlines())
-		# 正则表达式匹配出 id 信息
-		deviceId = re.findall(r'^\w*\b', readDeviceId[1])[0]
+		# readDeviceId = list(os.popen('adb devices').readlines())
+		# # 正则表达式匹配出 id 信息
+		# deviceId = re.findall(r'^\w*\b', readDeviceId[1])[0]
 		#设备信息
 		desired_caps={
-		'newCommandTimeout':60,
+		'newCommandTimeout':600,
 		'device':'android',
 		'platformName':'Android',
 		'browserName':'',
 		'version':'',
-		'deviceName':deviceId,
+		'deviceName':'',
 		'appPackage':'com.xywy.askxywy',
 		'appActivity':'.domain.welcome.activity.WelcomeActivity',
 		'unicodeKeyboard':True,
@@ -31,19 +32,35 @@ class Test_appnium:
 		for tries in range(maxTryNum):
 			try:	
 				self.driver = webdriver.Remote('http://localhost:4723/wd/hub',desired_caps)
-				break
 			except:
 				if tries < (maxTryNum-1):
-					print("重试第%d次") %maxTryNum
+					print("连接失败，正在重试")
 					continue
 				else:
-					print('启动webdriver失败')
+					print('连接手机失败，请尝试重新启动appium客户端')
+					sys.exit()
 			else:
-				logging.error("Has tried %d times, all failed!",maxTryNum)
+				time.sleep(5)
+				self.driver.implicitly_wait(2)
+				print("连接成功")
+				# 登陆
+				account = '17810354797'
+				passwd = 'test123'
+				self.fid('com.xywy.askxywy:id/tabMine').click()
+				# 判断登陆
+				try:
+					logined = self.fid('com.xywy.askxywy:id/mine_phone')
+				except:
+					# 未登陆，登陆
+					account_pwd = self.fids('com.xywy.askxywy:id/input_edit_text')
+					account_pwd[0].send_keys(account)
+					account_pwd[1].send_keys(passwd)
+					self.fid('com.xywy.askxywy:id/login_btn_tv').click()
+				else:
+					# 已登陆，跳过
+					self.fid('com.xywy.askxywy:id/ivTabIconLogo').click()
+				print("登陆完毕")
 				break
-		time.sleep(5)
-		self.driver.implicitly_wait(2)
-		print("程序启动完毕")
 
 	#获得机器屏幕大小x,y
 	def getSize(self):
@@ -91,4 +108,16 @@ class Test_appnium:
 	def mquit(self):
 		#退出driver
 		self.driver.quit()
-		print('程序退出')
+		os.system('adb shell am force-stop com.xywy.askxywy')
+		print('程序已退出')
+
+	def is_success(self):
+		#点击创建订单存在失败情况，需要重试逻辑及返回成功与失败
+		try:
+			pass
+		except Exception as e:
+			raise
+		else:
+			pass
+		finally:
+			pass
